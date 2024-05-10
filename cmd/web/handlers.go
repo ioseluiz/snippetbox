4822d0e1e54fd098b5e146308508b034a3c7c003
Jose Luis Muñoz <42367982+ioseluiz@users.noreply.github.com>
2024-05-10 12:38:13 -0500

    Home page template added

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 // Define a home handler function which returns a  byte slice
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox"))
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Use the template.ParseFiles function to read the template file into a
+	// a template set. If there's an error, we log the detailed error message and use the
+	// http.Error() function to send a generic 500 Internal Server Error
+	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	//We then use the Execute() method on the template set to write the template
+	// content as the response body. The last parameter to Execute() represents any
+	// dynamic data that we want to pass in.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 // Add a showSnippet handler function
