cc4773860bc9c2b342851579abb8ff47e4e7b96f
Jose Luis <joseluism1412@gmail.com>
2024-05-12 21:01:38 -0500

    MySQL Database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Define an application struct to hold the application wide dependencies
@@ -17,6 +20,9 @@ type application struct {
 func main() {
 	// Define a new comnnad line flag with the name 'addr'
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Define a new command line flag for the MySQL DSN string
+	dsn := flag.String("dsn", "web:jlmm1412@/snippetbox?parseTime=true", "MYSQL data source name")
+
 	//Parse the command line flag with flag.Parse() function
 	flag.Parse()
 	// Use log.New() to create a logger for writing information messages.
@@ -24,6 +30,15 @@ func main() {
 
 	// Create a logger for writing error messages.
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exists
+	defer db.Close()
 
 	// Initialize  a new instance of application containing the dependencies
 	app := &application{
@@ -39,7 +54,20 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
+	// Because the err variable is now alredy declared in the code above
+	// we need to use the assignment
 	// Call the ListenAndServe() method on our new http.Server struct
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
