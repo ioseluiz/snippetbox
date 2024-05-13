ce5e752867300c83581afb5a896a912c5b79da37
Jose Luis Muñoz <42367982+ioseluiz@users.noreply.github.com>
2024-05-13 07:45:09 -0500

    Database models added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/ioseluiz/snippetbox_3/pkg/models/mysql"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -15,6 +17,7 @@ import (
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
@@ -44,6 +47,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	// Initialize a new http.Server struct.
